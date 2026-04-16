d756d24a514b38f99b8f03fef65ef79f4788624c
Krishna-Jonnala <krishnajonnala33@gmail.com>
2026-04-16 16:43:04 +0530

    Added UC16

---
+++ App/src/main/UseCase16SortCapacity.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class UseCase16SortCapacity {
+
+    public static List<Integer> sort(List<Integer> list) {
+        Collections.sort(list);
+        return list;
+    }
+}
